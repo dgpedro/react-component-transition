3018158976194a65bcfcf8f3e20fe28c6b0e21b2
Diogo Pedro <diogo.e.pedro@gmail.com>
2021-11-15 22:55:48 +0100

    Add show test

--- cypress/integration/hide.ts
+++ cypress/integration/hide.ts
@@ -1,13 +1,27 @@
 describe("Hide", () => {
-    it("Slide right", () => {
-        cy.visit("http://localhost:9000/show-hide")
+    it("Hide", () => {
+        cy.visit("http://localhost:9000/hide")
             .then(() => {
                 cy.get("#slide-right-check").click();
                 
                 cy.wait(800);
                 
                 cy.get("#slide-right").toMatchImageSnapshot();
-                cy.get("#slide-right").contains("#slide-right-box").should('not.exist');
+                cy.get("#slide-right-box").should('not.exist');
+            })
+    });
+
+    it("Show", () => {
+        cy.visit("http://localhost:9000/hide")
+            .then(() => {
+                cy.get("#slide-right-check").click();
+                cy.wait(1100);
+                
+                cy.get("#slide-right-check").click();
+                cy.wait(300);
+                
+                cy.get("#slide-right").toMatchImageSnapshot();
+                cy.get("#slide-right-box").should('exist');
             })
     });
 });
